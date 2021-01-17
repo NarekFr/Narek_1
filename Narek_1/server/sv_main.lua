ff749fbe4ff6ae9da29d5f16f98435bc486511be
GRZ <64192403+GRZ-D3V@users.noreply.github.com>
2021-01-17 17:31:26 -0500

    Uptade setJob server side

--- Narek_1/server/sv_main.lua
+++ Narek_1/server/sv_main.lua
@@ -1,4 +1,5 @@
 ESX             = nil
+local availableJobs = {}
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
@@ -45,10 +46,34 @@ AddEventHandler('Narek_1:Shop', function(id, label, item, price)
     end
 end)
 
-RegisterServerEvent('Narek_1:Autre')
-AddEventHandler('Narek_1:Autre', function(label, job)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    
-    TriggerClientEvent('esx:showAdvancedNotification', source, _U('poleemploi'), '', _U('SetJob')..label, 'CHAR_DOM', 1)
-    xPlayer.setJob(job, 0) 
+
+MySQL.ready(function()
+	MySQL.Async.fetchAll('SELECT name, label FROM jobs WHERE whitelisted = @whitelisted', {
+		['@whitelisted'] = false
+	}, function(result)
+		for i=1, #result, 1 do
+			table.insert(availableJobs, {
+				job = result[i].name,
+				label = result[i].label
+			})
+		end
+	end)
+end)
+
+ESX.RegisterServerCallback('esx_joblisting:getJobsList', function(source, cb)
+	cb(availableJobs)
+end)
+
+RegisterServerEvent('esx_joblisting:setJob')
+AddEventHandler('esx_joblisting:setJob', function(job)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	if xPlayer then
+		for k,v in ipairs(availableJobs) do
+			if v.job == job then
+				xPlayer.setJob(job, 0)
+				break
+			end
+		end
+	end
 end)
